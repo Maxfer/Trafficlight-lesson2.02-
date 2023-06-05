c3e1bdadcb72d39b13fbd9172f5a04dc0d8af51a
Maxfer <maximus604@gmail.com>
2023-06-05 14:04:14 +0300

    Initial Commit

---
+++ Trafficlight(lesson 2.02)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trafficlight(lesson 2.02)
+//
+//  Created by Максим Шкрябин on 05.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
