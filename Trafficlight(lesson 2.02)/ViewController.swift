319d782098d34f0cab2cde272e8fd7f0a490f483
Maxfer <maximus604@gmail.com>
2023-06-05 15:46:26 +0300

    setup view and button

--- Trafficlight(lesson 2.02)/ViewController.swift
+++ Trafficlight(lesson 2.02)/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 100
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 100
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 100
+        greenLightView.alpha = 0.3
+        
     }
 
-
+    @IBAction func lightButtonDidTapped() {
+    }
+    
 }
 
