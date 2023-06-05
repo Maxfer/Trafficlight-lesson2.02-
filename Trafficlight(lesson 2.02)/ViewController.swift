a4cb329c3d98f25eef88fde0bb5cf06336321fe7
Maxfer <maximus604@gmail.com>
2023-06-05 16:48:20 +0300

    setup logic button

--- Trafficlight(lesson 2.02)/ViewController.swift
+++ Trafficlight(lesson 2.02)/ViewController.swift
@@ -26,10 +26,27 @@ class ViewController: UIViewController {
         greenLightView.layer.cornerRadius = 100
         greenLightView.alpha = 0.3
         
+        lightButton.layer.cornerRadius = 30
     }
 
-    @IBAction func lightButtonDidTapped() {
+    @IBAction func lightButtonDidTapprd() {
+        
+        if lightButton.currentTitle == "Start" {
+            lightButton.setTitle("Next", for: .normal)
+            redLightView.alpha = 1
+        } else if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        }
+        
     }
+    
     
 }
 
